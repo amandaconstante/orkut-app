711b64bfb0f404f3c7e0d97736d48092c1fc902d
Amanda <89360779+amandaconstante@users.noreply.github.com>
2026-04-13 15:36:08 -0300

    chore: configurar conexão com banco de dados postgres

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const {Pool} = require('pg');
+
+module.exports = new Pool({
+    //connectionString pq está sendo usado a Database_url longa
+    connectionString: process.env.DATABASE_URL
+
+    // se fosse feito linha por linha no env (separar host, senha etc)
+    // seria:
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST
+
+
+})

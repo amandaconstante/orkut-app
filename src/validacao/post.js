315bd0f118ad5cbeca5bc9403a463929c51e1f6f
Amanda <89360779+amandaconstante@users.noreply.github.com>
2026-04-28 10:11:53 -0300

    feat: implementar validação de posts com joi

---
+++ src/validacao/post.js
@@ -0,0 +1,34 @@
+const Joi = require('joi');
+
+
+const postSchema = Joi.object({
+    titulo: Joi.string().min(3).required().messages({
+        "string.empty": "O Título é obrigatório",
+        "string.min": "O Título deve ter pelo menos 3 caracteres",
+        "any.required": "O Título é obrigatório"
+    }),
+    conteudo: Joi.string().min(5).required().messages({
+        "string.empty": "O conteúdo do post é obrigatório",
+        "string.min": "O conteúdo deve ter pelo menos 5 caracteres",
+        "any.required": "O conteúdo é obrigatório"
+    }),
+    usuario_id: Joi.number().integer().required().messages({
+        "number.base": "O usuario_id deve ser um número",
+        "number.integer": "O usuario_id deve ser um número inteiro",
+        "any.required": "O usuario_id é obrigatório"
+    })
+});
+
+function validarPost(req, res, next) {
+    const {error} = postSchema.validate(req.body, { abortEarly: false });
+
+    if (error) {
+        console.log("erro = " + error);
+        return res.status(400).json({
+            erro: error.details.map(e => e.message)
+        });
+    }
+    next();
+}
+
+module.exports = validarPost;
